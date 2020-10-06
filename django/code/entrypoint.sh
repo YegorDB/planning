2c0f4c1a3cb7cd8d9664611d9b9287f86b008fd6
Yegor Bitensky <yegorbitensky@gmail.com>
2020-10-07 01:13:03 +0300

    add django project

--- django/code/entrypoint.sh
+++ django/code/entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-while true
+until python manage.py inspectdb &> /dev/null
 do
-	sleep 60
+    echo "Waiting for db ..."
+    sleep 5
 done
+
+python manage.py migrate
+python manage.py collectstatic --noinput
+python manage.py runserver 0.0.0.0:8000
