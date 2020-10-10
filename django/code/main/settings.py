286d9bac7e2f23914dcfb371975ab0a692eea916
Yegor Bitensky <yegorbitensky@gmail.com>
2020-10-10 23:38:57 +0300

    Add users app

--- django/code/main/settings.py
+++ django/code/main/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
     'plann',
 ]
 
@@ -110,3 +111,6 @@ STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+
+AUTH_USER_MODEL = 'users.User'
