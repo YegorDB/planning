251dc9e26ea9345b42b7f9e922541f6fa7407b19
Yegor Bitensky <yegorbitensky@gmail.com>
2020-10-18 01:21:08 +0300

    Add tasks status

--- django/code/plann/migrations/0001_initial.py
+++ django/code/plann/migrations/0001_initial.py
@@ -21,6 +21,7 @@ class Migration(migrations.Migration):
                 ('deadline', models.DateTimeField()),
                 ('duration', models.DurationField()),
                 ('priority', models.SmallIntegerField(choices=[(0, 'Not set'), (1, 'Low'), (2, 'Normal'), (3, 'High'), (4, 'Critical')], default=0)),
+                ('status', models.CharField(choices=[('NS', 'Not set'), ('IP', 'In progress'), ('DN', 'Done'), ('CL', 'Canceled')], default='NS', max_length=2)),
                 ('creator', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='created_tasks', to=settings.AUTH_USER_MODEL)),
                 ('depends_on', models.ManyToManyField(related_name='_task_depends_on_+', to='plann.Task')),
                 ('parent', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, related_name='children', to='plann.task')),
--- django/code/plann/models.py
+++ django/code/plann/models.py
@@ -12,6 +12,12 @@ class Task(models.Model):
         HIGH = 3, _('High')
         CRITICAL = 4, _('Critical')
 
+    class Status(models.TextChoices):
+        NOT_SET = 'NS', _('Not set')
+        IN_PROGRESS = 'IP', _('In progress')
+        DONE = 'DN', _('Done')
+        CANCELED = 'CL', _('Canceled')
+
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, default='')
     creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='created_tasks')
@@ -21,3 +27,4 @@ class Task(models.Model):
     priority = models.SmallIntegerField(choices=Priority.choices, default=Priority.NOT_SET)
     depends_on = models.ManyToManyField('self')
     parent = models.ForeignKey('self', on_delete=models.CASCADE, related_name='children', blank=True, null=True)
+    status = models.CharField(max_length=2, choices=Status.choices, default=Status.NOT_SET)
