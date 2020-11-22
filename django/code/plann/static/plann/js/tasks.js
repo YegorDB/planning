85a5ed15f3205bf6003d661eabbf120001431ad6
Yegor Bitensky <yegorbitensky@gmail.com>
2020-11-23 00:28:26 +0500

    Add tasks diagram scale lines

--- django/code/plann/static/plann/js/tasks.js
+++ django/code/plann/static/plann/js/tasks.js
@@ -1,3 +1,25 @@
+/** Draw tasks diagram scale. */
+function drawScale() {
+  let ctx = document.getElementById("scale").getContext("2d");
+
+  ctx.fillStyle = "black";
+  ctx.beginPath();
+  for (let hour = 0; hour < 24; hour++) {
+    let x = hour * 60 + 1;
+
+    ctx.moveTo(x, 0);
+    ctx.lineTo(x, 10);
+
+    for (let minute = 10; minute < 60; minute += 10) {
+      ctx.moveTo(x + minute, 5);
+      ctx.lineTo(x + minute, 10);
+    }
+  }
+
+  ctx.stroke();
+}
+
+
 $(document).ready(function() {
   $.ajax({
     url: '/api/1.0/user_tasks/',
@@ -5,4 +27,6 @@ $(document).ready(function() {
   .done((data) => {
     $('#tasks-box').text(JSON.stringify(data));
   });
+
+  drawScale();
 });
