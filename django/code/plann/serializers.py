23b6d396eceb142b3a71a80c7acb606a9a9b1e74
Yegor Bitensky <yegorbitensky@gmail.com>
2020-12-12 19:17:47 +0500

    Add task creator serializer

--- django/code/plann/serializers.py
+++ django/code/plann/serializers.py
@@ -1,9 +1,20 @@
+from django.contrib.auth import get_user_model
+
 from rest_framework import serializers
 
 from plann.models import Task
 
 
+class TaskRelatedUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ['id', 'username', 'first_name', 'last_name']
+
+
 class TaskSerializer(serializers.ModelSerializer):
+    creator = TaskRelatedUserSerializer()
+    responsible = TaskRelatedUserSerializer()
+
     class Meta:
         model = Task
         fields = '__all__'
