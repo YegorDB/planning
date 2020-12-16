fefe16a286fcabf0bdc34edd23b3fd76b23fb7eb
Yegor Bitensky <yegorbitensky@gmail.com>
2020-12-17 01:44:22 +0500

    Add nginx

--- django/code/main/urls.py
+++ django/code/main/urls.py
@@ -1,5 +1,4 @@
 from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -16,5 +15,4 @@ urlpatterns = [
     path('', include('plann.urls')),
     path('admin/', admin.site.urls),
     path('api/', include(api_urlpatterns)),
-    *static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 ]
