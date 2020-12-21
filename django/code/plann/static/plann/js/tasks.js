61c4547e45443be7c3a29dfdb0d6f62129e9e9b5
Yegor Bitensky <yegorbitensky@gmail.com>
2020-12-22 03:19:21 +0500

    Add stack styles

--- django/code/plann/static/plann/js/tasks.js
+++ django/code/plann/static/plann/js/tasks.js
@@ -5,43 +5,38 @@ $(document).ready(function() {
     url: '/api/1.0/user_tasks/',
   })
   .done((data) => {
-    let headerItem = document.createElement('div');
-    let headerNames = ['Name', 'Priority', 'Status'];
-    for (let headerName of headerNames) {
-      let headerItemValue = document.createElement('div');
-      $(headerItemValue).addClass('tasks-stack-cell');
-      $(headerItemValue).text(headerName);
-      $(headerItem).append(headerItemValue);
-    }
-    $('#tasks-stack').append(headerItem);
-
     for (let task of data) {
       let taskItem = document.createElement('div');
       $(taskItem).addClass('tasks-stack-item');
 
       let taskItemName = document.createElement('div');
-      $(taskItemName).addClass('tasks-stack-cell tasks-stack-item-name');
-      $(taskItemName).text(task.name);
+      $(taskItemName).addClass('tasks-stack-cell tasks-stack-cell-name');
+      let taskItemNameText = document.createElement('div');
+      $(taskItemNameText).addClass('tasks-stack-cell-name-text');
+      $(taskItemNameText).text(task.name);
       if (task.description && task.description != '') {
-        $(taskItemName).append(`
+        $(taskItemNameText).append(`
           <div class="tasks-stack-item-description" title="${task.description}">
             <div>i</div>
           </div>
         `);
       }
+      $(taskItemName).append(taskItemNameText);
       $(taskItem).append(taskItemName);
 
       let taskItemPriority = document.createElement('div');
-      $(taskItemPriority).addClass('tasks-stack-cell');
+      $(taskItemPriority).addClass('tasks-stack-cell tasks-stack-cell-priority');
       $(taskItemPriority).text(CHOISES.task.priority[task.priority]);
       $(taskItem).append(taskItemPriority);
 
       let taskItemStatus = document.createElement('div');
-      $(taskItemStatus).addClass('tasks-stack-cell');
+      $(taskItemStatus).addClass('tasks-stack-cell tasks-stack-cell-status');
       $(taskItemStatus).text(CHOISES.task.status[task.status]);
       $(taskItem).append(taskItemStatus);
 
-      $('#tasks-stack').append(taskItem);
+      $('#tasks-stack-items').append(taskItem);
     }
+
+    console.log('data', data);
   });
 });
