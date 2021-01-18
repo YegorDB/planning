464c4d23c810eda694cb5e2479bbe5be2b90b96e
Yegor Bitensky <yegorbitensky@gmail.com>
2021-01-19 02:24:42 +0500

    Add tasks page css files builders

--- src/node/gulpfile.js
+++ src/node/gulpfile.js
@@ -1,12 +1,29 @@
-const { src, dest } = require('gulp');
-const uglify = require('gulp-uglify-es').default;
-const rename = require('gulp-rename');
+const { dest, parallel, src } = require('gulp');
 const browserify = require('browserify');
-const source = require('vinyl-source-stream');
 const buffer = require('vinyl-buffer');
+const csso = require('gulp-csso');
+const rename = require('gulp-rename');
+const source = require('vinyl-source-stream');
+const uglify = require('gulp-uglify-es').default;
 
 
-exports.tasks = function() {
+exports.base = function() {
+  return src('src/plann/css/base.css')
+    .pipe(csso())
+    .pipe(rename({ extname: '.min.css' }))
+    .pipe(dest('output/plann/css/'));
+}
+
+
+exports.dialog = function() {
+  return src('src/plann/css/dialog.css')
+    .pipe(csso())
+    .pipe(rename({ extname: '.min.css' }))
+    .pipe(dest('output/plann/css/'));
+}
+
+
+function tasksJS() {
   return browserify('src/plann/js/tasks/main.js')
     .bundle()
     .pipe(source('tasks.js'))
@@ -15,3 +32,14 @@ exports.tasks = function() {
     .pipe(rename({ extname: '.min.js' }))
     .pipe(dest('output/plann/js/'));
 }
+
+
+function tasksCSS() {
+  return src('src/plann/css/tasks.css')
+    .pipe(csso())
+    .pipe(rename({ extname: '.min.css' }))
+    .pipe(dest('output/plann/css/'));
+}
+
+
+exports.tasks = parallel(tasksJS, tasksCSS);
