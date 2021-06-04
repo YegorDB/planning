52d27995828a41c4cb52d3910420d7f97f3b5b63
Yegor Bitensky <yegorbitensky@gmail.com>
2021-06-04 23:50:01 +0300

    Add task status changing dialog component

--- src/gulp/src/plann/js/tasks/main.jsx
+++ src/gulp/src/plann/js/tasks/main.jsx
@@ -3,6 +3,81 @@ const { TasksStatusChangingDialog, TasksStatusFilterDialog, TasksPriorityFilterD
 const { TasksCreation } = require('./creation');
 
 
+
+class TasksStatusChangingDialogComponent extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      taskItem: null,
+    };
+
+    this._click = (e) => {
+      this.setState({ taskItem: null });
+    };
+
+    this._changeStatusStart = (e) => {
+      $('#change-status-dialog').addClass('dialog-window-open');
+      this.setState({ taskItem: e.taskItem });
+    };
+  }
+
+  componentDidMount() {
+    $('#change-status-dialog').on('click', this._click);
+    $('#change-status-dialog').on('changeStatusStart', this._changeStatusStart);
+  }
+
+  componentWillUnmount() {
+    $('#change-status-dialog').off('click', this._click);
+    $('#change-status-dialog').off('changeStatusStart', this._changeStatusStart);
+  }
+
+  render() {
+    return Object.entries(CHOISES.task.status).map((s) =>
+      <div className={`
+             tasks-stack-change-status-dialog-item
+             tasks-stack-item-status
+             tasks-stack-item-status-${s[0].toLowerCase()}
+           `}
+           onClick={(e) => { this._changeValue(s[0]); }}>
+        {s[1]}
+      </div>
+    );
+  }
+
+  /**
+   * Change status value.
+   * @param {string} value - New status value.
+   */
+  _changeValue(value) {
+    if (!this._taskItem) return;
+
+    WAIT_SCREEN.enable();
+    $.ajax({
+      url: URLS.update_task.replace(/\d+\/$/, `${this._taskItem.id}/`),
+      data: JSON.stringify({
+        'status': value,
+      }),
+      type: 'PATCH',
+      contentType: 'application/json',
+    })
+    .done((taskData) => {
+      this._taskItem.status.value = value;
+    })
+    .fail((jqXHR, textStatus, errorThrown) => {
+      console.log('jqXHR', jqXHR);
+    })
+    .always(() => {
+      this._taskItem = null;
+      WAIT_SCREEN.disable();
+    });
+
+    $('#change-status-dialog').removeClass('dialog-window-open');
+  }
+}
+
+
+
 window.CHOISES = JSON.parse(RAW_CHOISES);
 window.URLS = JSON.parse(RAW_URLS);
 window.WAIT_SCREEN = {
