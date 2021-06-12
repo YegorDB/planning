90e8372c23052da92023022ae88ec5b5579d14f8
Yegor Bitensky <yegorbitensky@gmail.com>
2021-06-13 01:21:18 +0300

    Add task stack item meaning description component

--- src/gulp/src/plann/js/tasks/stack/item/meaning.jsx
+++ src/gulp/src/plann/js/tasks/stack/item/meaning.jsx
@@ -1,3 +1,6 @@
+const classNames = require('classnames');
+
+
 /** Tasks stack item meaning name. */
 class Name extends React.Component {
 
@@ -13,24 +16,6 @@ class Name extends React.Component {
   }
 
   /**
-   * Task name value.
-   * @return {string} Value.
-   */
-  get value() {
-    return this.state.value;
-  }
-
-  /**
-   * Set task name value.
-   * @param {string} value - Value.
-   */
-  set value(value) {
-    this.setState({
-      value: value,
-    });
-  }
-
-  /**
    * Render stack item meaning name.
    * @returns {React.Element}
    */
@@ -38,6 +23,43 @@ class Name extends React.Component {
     return (
       <div className="tasks-stack-cell-meaning-name" >
         this.state.value
+      </div>
+    );
+  }
+}
+
+
+/** Tasks stack item meaning description. */
+class Decsription extends React.Component {
+
+  /**
+   * Creation.
+   * @param {integer} props.value - Task description value.
+   */
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: props.value,
+    };
+  }
+
+  get showed() {
+    return this.state.value && this.state.value != '';
+  }
+
+  /**
+   * Render stack item meaning description.
+   * @returns {React.Element}
+   */
+  render() {
+    let classes = classNames('tasks-stack-cell-meaning-description', {
+      'tasks-stack-cell-meaning-description-show': this.showed,
+    });
+
+    return (
+      <div className={classes}
+           title={ this.showed ? this.state.value : '' } >
+        <div>i</div>
       </div>
     );
   }
