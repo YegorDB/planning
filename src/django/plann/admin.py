a02b26a47fcdc5047f9f9afca21b45df5a73fd8c
Yegor Bitensky <yegorbitensky@gmail.com>
2021-07-02 00:28:57 +0300

    Add tags admin model

--- src/django/plann/admin.py
+++ src/django/plann/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from plann.models import Tag
+
+
+class TagAdmin(admin.ModelAdmin):
+    fields = ('name',)
+    list_display = ('name',)
+    search_fields = ('^name',)
+
+    def has_view_permission(self, request, obj=None):
+        return request.user.is_superuser
+
+    def has_add_permission(self, request):
+        return request.user.is_superuser
+
+    def has_change_permission(self, request, obj=None):
+        return request.user.is_superuser
+
+    def has_delete_permission(self, request, obj=None):
+        return request.user.is_superuser
+
+
+admin.site.register(Tag, TagAdmin)
