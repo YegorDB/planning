3984b012200d3a4bf67aa62bc39ad5b029e4d5f5
Yegor Bitensky <yegorbitensky@gmail.com>
2021-09-21 23:23:07 +0300

    Improve gulp handlers #3

--- src/gulp/gulpfile.js
+++ src/gulp/gulpfile.js
@@ -9,34 +9,47 @@ const touch = require('gulp-touch-custom');
 const uglify = require('gulp-uglify-es').default;
 
 
-function tasksJS() {
-  return browserify('src/plann/js/tasks/main.jsx')
-    .transform('babelify', {
-      plugins: ['transform-react-jsx']
-    })
-    .bundle()
-    .pipe(source('tasks.js'))
-    .pipe(buffer())
-    .pipe(uglify())
-    .pipe(rename({ extname: '.min.js' }))
-    .pipe(dest('output/plann/js/'));
+function getJSHandler(app, path) {
+  return function() {
+    return browserify(`src/${app}/js/${path}/main.jsx`)
+      .transform('babelify', {
+        plugins: ['transform-react-jsx']
+      })
+      .bundle()
+      .pipe(source(`${path}.js`))
+      .pipe(buffer())
+      .pipe(uglify())
+      .pipe(rename({ extname: '.min.js' }))
+      .pipe(dest(`output/${app}/js/`));
+  }
 }
 
+let taskJS = getJSHandler('plann', 'task');
+let tasksJS = getJSHandler('plann', 'tasks');
 
-function tasksCSS() {
-  return src('src/plann/less/tasks/main.less')
-    .pipe(less())
-    .pipe(csso())
-    .pipe(rename('tasks.min.css'))
-    .pipe(dest('output/plann/css/'))
-    .pipe(touch());
+
+function getCSSHandler(app, path) {
+  return function() {
+    return src(`src/${app}/less/${path}/main.less`)
+      .pipe(less())
+      .pipe(csso())
+      .pipe(rename(`${path}.min.css`))
+      .pipe(dest(`output/${app}/css/`))
+      .pipe(touch());
+  }
 }
+
+let taskCSS = getCSSHandler('plann', 'task')
+let tasksCSS = getCSSHandler('plann', 'tasks')
 
 
 exports.default = function() {
+  watch('src/plann/js/task/**/*.(js|jsx)', taskJS);
+  watch('src/plann/less/task/**/*.less', taskCSS);
   watch('src/plann/js/tasks/**/*.(js|jsx)', tasksJS);
   watch('src/plann/less/tasks/**/*.less', tasksCSS);
 };
 
 
+exports.task = parallel(taskJS, taskCSS);
 exports.tasks = parallel(tasksJS, tasksCSS);
