b08c1b88d8a37f7c704a0378be2bfde99d76ff21
Yegor Bitensky <yegorbitensky@gmail.com>
2021-09-27 23:40:07 +0300

    Add tasks item click handler

--- src/gulp/src/plann/js/tasks/items/item/main.jsx
+++ src/gulp/src/plann/js/tasks/items/item/main.jsx
@@ -7,18 +7,30 @@ const { ItemStatus } = require('./status.jsx');
 /** Tasks item. */
 class Item extends React.Component {
 
+  /** Creation. */
+  constructor(props) {
+    super(props);
+
+    this._handleClick = this._handleClick.bind(this);
+  }
+
   /**
    * Render.
    * @returns {React.Element}
    */
   render() {
     return (
-      <div className="task-item" >
+      <div className="task-item"
+           onClick={ this._handleClick } >
         <ItemPriority value={ this.props.taskData.priority } />
         <ItemMeaning name={ this.props.taskData.name } />
         <ItemStatus value={ this.props.taskData.status } />
       </div>
     );
+  }
+
+  _handleClick() {
+    document.location.assign(`/task/${this.props.taskData.id}/`);
   }
 }
 
